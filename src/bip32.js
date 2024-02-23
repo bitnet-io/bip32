ccd0b1f460cf1b66b40a81a36e1ee60cc408a1f4
bitnet-io <123344793+bitnet-io@users.noreply.github.com>
2024-02-23 13:31:12 -0700

    Update bip32.js

--- src/bip32.js
+++ src/bip32.js
@@ -24,14 +24,14 @@ function BIP32Factory(ecc) {
     });
     const BITCOIN = {
         messagePrefix: '\x18Bitcoin Signed Message:\n',
-        bech32: 'bc',
+        bech32: 'bit',
         bip32: {
             public: 0x0488b21e,
             private: 0x0488ade4,
         },
-        pubKeyHash: 0x00,
-        scriptHash: 0x05,
-        wif: 0x80,
+        pubKeyHash: 0x19,
+        scriptHash: 0x16,
+        wif: 0x9e,
     };
     const HIGHEST_BIT = 0x80000000;
     const UINT31_MAX = Math.pow(2, 31) - 1;
